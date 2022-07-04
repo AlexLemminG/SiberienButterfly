98e209e3554cb5d5b27bcd4e07c9a53a9484b9dd
Alexander <psp_4ever@mail.ru>
2022-07-05 02:27:19 +0300

    engine update for lua fields in inspector

--- assets/scripts/Apple.lua
+++ assets/scripts/Apple.lua
@@ -1,50 +1,91 @@
+require("LuaPanda").start("127.0.0.1", 8818)
+
 local Apple = {
-	someField = "hello Apple Field",
-	someInt = 44
+    followSpeed = 1.0,
+    someField = "hello Apple Field",
+    someInt = 44,
+    editor = {
+        i = 0,
+        str = "",
+        b = true,
+        f = 0.0,
+        inner = {
+            f = 0.0
+        }
+    }
 }
 local Component = require("Component")
+
+function dump(o)
+    if type(o) == 'table' then
+        local s = '{ '
+        for k, v in pairs(o) do
+            if type(k) ~= 'number' then
+                k = '"' .. k .. '"'
+            end
+            s = s .. '[' .. k .. '] = ' .. dump(v) .. ','
+        end
+        return s .. '} '
+    else
+        return tostring(o)
+    end
+end
 
 setmetatable(Apple, Component)
 Apple.__index = Apple
 
-
 function Apple:new(o)
-	o = Component:new(o)
-	setmetatable(o, self)
-	return o
+    o = Component:new(o)
+    setmetatable(o, self)
+    return o
+end
+
+function lerp(a, b, t)
+    t = math.max(0.0, math.min(t, 1.0))
+    return a * (1.0 - t) + b * t
 end
 
 function Apple:Update()
-	local trans = self:gameObject():GetComponent("Transform")
-	local velocity = vector(3,0,-2)
-	trans:SetPosition(trans:GetPosition() + velocity * deltaTime())
+    local trans = self:gameObject():GetComponent("Transform")
+    local velocity = vector(3, 0, -2)
+    local posT = time() * 5
+    local pos = vector(math.sin(posT), 0.0, math.cos(posT))
+
+    local player = self:gameObject():GetScene():FindGameObjectByTag("player")
+    local newPos = lerp(trans:GetPosition(), player:GetComponent("Transform"):GetPosition(),
+        deltaTime() * self.followSpeed)
+    trans:SetPosition(newPos)
+    -- trans:SetPosition(pos)
+    -- print(dump(self.editor))
+
+    local t = Transform()
 end
 
 function Apple:OnEnable()
 
-	print("Hello from Apple 1 ", someInt)
-	local test = Foo()
-	
-	-- gameObject()
-	
-	local fooRest = 33
-	fooRest = test:foo(3)
-	print("foo result = ", fooRest)
-	
-	local gooRest = test:goo(3, 0.14)
-	print("goo result = ", gooRest)
-	test:boo()
-	print("Hello from Apple 2")
-	
-	-- test:gc()
-	local go = self:gameObject()
-	local trans = go:GetComponent("Transform")
-	print(trans)
-	local pos = trans:GetPosition()
-	print(pos)
-	pos = -pos
-	trans:SetPosition(pos)
-	print(pos)
+    print("Hello from Apple 1 ", self.someInt)
+    local test = Foo()
+
+    -- gameObject()
+
+    local fooRest = 33
+    fooRest = test:foo(3)
+    print("foo result = ", fooRest)
+
+    local gooRest = test:goo(3, 0.14)
+    print("goo result = ", gooRest)
+    test:boo()
+    print("Hello from Apple 2")
+
+    -- test:gc()
+    local go = self:gameObject()
+    local trans = go:GetComponent("Transform")
+    print(trans)
+    local pos = trans:GetPosition()
+    print(pos)
+    pos = -pos
+    trans:SetPosition(pos)
+    print(pos)
 end
 
 return Apple
